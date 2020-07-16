2dcb63f9f22ad4f0a9cb4734404a9b34d1c49822
PRAZERES SANTOS Ilton Karly <iltonk.si@gmail.com>
2020-07-16 16:32:25 +0100

    adjust of the configurations

--- back-end/src/shared/infra/server.ts
+++ back-end/src/shared/infra/server.ts
@@ -1,12 +1,14 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (request: Request, response: Response) => {
-  return response.json({ "Version-API": '0.1', "Author": "Ilton Karly Prazeres dos Santos" })
-})
+  return response.json({
+    'Version-API': '0.1',
+    Author: 'Ilton Karly Prazeres dos Santos',
+  });
+});
 
 app.listen('3333', () => {
-  console.log('servidor starded')
-})
-
+  console.log('servidor starded');
+});
