943af8a59fbcbb92c09fc4ddfab487959e8e4ba8
PRAZERES SANTOS Ilton Karly <iltonk.si@gmail.com>
2020-07-16 16:16:54 +0100

    starting the configurations and structure

---
+++ back-end/src/shared/infra/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (request: Request, response: Response) => {
+  return response.json({ "Version-API": '0.1', "Author": "Ilton Karly Prazeres dos Santos" })
+})
+
+app.listen('3333', () => {
+  console.log('servidor starded')
+})
+
